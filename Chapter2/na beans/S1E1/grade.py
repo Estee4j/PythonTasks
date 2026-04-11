0aebb47e43a134f8046fd81ae4536f1f578eec00
Estee4j <estherokagwa@gmail.com>
2026-04-11 18:52:23 +0100

    Added a new folder

---
+++ Chapter2/na beans/S1E1/grade.py
@@ -0,0 +1,17 @@
+number = input("Enter your score: ")
+number = int(number)
+
+if(number >= 90):
+    print("You scored A")
+
+elif(number >= 80 or number >= 89):
+    print("you scored B")
+
+elif(number >= 70 or number >= 79):
+    print("you scored C")
+
+elif(number >= 60 or number >= 69):
+    print("you scored D")
+
+else:
+    print("you scored F")
---
+++ Chapter2/na beans/S1E1/hottea.py
@@ -0,0 +1,33 @@
+crash course on how to make a cup of tea.
+
+get a cup of water and pour into a kettle.
+
+for electric kettle, you will have to plug into a switch/socket to let it heat up
+
+else, use cooking gas or stove to boil your water.
+
+allow water to heat for 10mins if you are using the boiler
+
+the boiling point should start from 10'C
+
+the cup of tea should be lesser or equal to 50'C
+
+increase the temperature by 10'C
+
+allow water to heat for 5mins if you are using stove or cooking gas.
+
+ after heating/boiling, turn off the gas cylinder and electric kettle.
+
+pour the boiled water into a cup
+
+if you are using a lipton, place your lipton inside your cup of boiled water and stir!!
+
+else, use your normal boiled water.
+
+pour your preffered beverage into your cup of boiled water
+
+stir it together
+
+and boom, you just made yourself a cup of tea.
+
+you don't need to thank me, what are friends for!!!
